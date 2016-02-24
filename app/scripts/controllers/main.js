c008cf06ff15a5fc105775caf01e1bf24c7e1763
don Onwunum <don@pigstycoders.com>
2016-02-24 15:17:17 +0100

    initial project commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAngularProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAngularProjectApp
+ */
+angular.module('myAngularProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
