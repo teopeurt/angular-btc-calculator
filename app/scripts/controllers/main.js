ab3f8638bdfddc8764eb3830c3cfbff2a60ba39b
don Onwunum <don@pigstycoders.com>
2016-02-24 17:54:58 +0100

    added investment table and investment

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,19 @@
  * Controller of the myAngularProjectApp
  */
 angular.module('myAngularProjectApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-       // attaching 0 to the DOM
-      $scope.somenumber = 320;
-  }]);
+  .controller('MainCtrl', function ($scope, $http) {
+          
+     // calling the api, grabbing the value for USD, appending it to the dom
+     $http.get("https://bitpay.com/api/rates")
+          .success(function(data){
+            $scope.rates = data;
+            for(var i=0;i<data.length;i++){
+              if (data[i].code == "USD"){
+                $scope.currRate = data[i].rate;
+              }
+            }
+            $scope.initalAmt  = 5000;
+            $scope.newAmt     = function(price){return price/$scope.currRate * $scope.initalAmt;}
+            $scope.profit     = function(price){return price/$scope.currRate * $scope.initalAmt - $scope.initalAmt;}
+      });      
+  });
